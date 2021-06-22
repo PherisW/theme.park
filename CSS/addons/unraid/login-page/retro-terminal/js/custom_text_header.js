358a7527b5b3a32032e0af3f35723651a076b81b
pherisw <heyblinx@gmail.com>
2021-06-21 19:42:10 -0500

    changed wallpaper(last)

--- CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -4,14 +4,13 @@
 // Needs to be inside <section id="login" class="shadow">
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>
-_____    __   .__                         _________
+<div class="custom-text-header"> <pre>_____    __   .__                         _________
 /  _  \ _/  |_ |  |  _____     ______     /   _____/  ____ _______ ___  __  ____ _______
 /  /_\  \\   __\|  |  \__  \   /  ___/     \_____  \ _/ __ \\_  __ \\  \/ /_/ __ \\_  __ \
 /    |    \|  |  |  |__ / __ \_ \___ \      /        \\  ___/ |  | \/ \   / \  ___/ |  | \/
 \____|__  /|__|  |____/(____  //____  >    /_______  / \___  >|__|     \_/   \___  >|__|
      \/                  \/      \/             \/      \/                    \/
-                                                                                         
+
 </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
